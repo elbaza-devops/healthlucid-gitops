2b82db2abe882fc4a39556c01fa9a1c2b6c96494
Ahmed elbaz <ahmedelbaz462@gmail.com>
2025-08-07 01:05:51 +0300

    Update URLs

--- scripts/secrets.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-kubectl create secret generic redis-master-secret --from-literal=REDIS_PASSWORD=<YOUR_REDIS_PASSWORD> --namespace dev
-kubectl create secret generic redis-master-secret --from-literal=REDIS_PASSWORD=<YOUR_REDIS_PASSWORD> --namespace staging
-kubectl create secret generic redis-master-secret --from-literal=REDIS_PASSWORD=<YOUR_REDIS_PASSWORD> --namespace prod
